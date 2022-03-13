48f1d97598040fe1b6ec025da81077a062acef85
Anand Jha <akjha96@gmail.com>
2022-03-13 12:01:47 +0530

    Created questions widget & implemented it in main widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
+//custom widget
+import 'question.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-  int questionIndex = 0;
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  int _questionIndex = 0;
 
   final questions = [
     'What\'s your favourite color?',
@@ -16,18 +27,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     void answerQuestion() {
-      questionIndex += 1;
-      print(questionIndex);
+      if (_questionIndex < questions.length - 1) {
+        setState(() {
+          _questionIndex += 1;
+        });
+        print(_questionIndex);
+      } else {
+        print('Maximum number of questions reached: ${_questionIndex + 1}');
+      }
     }
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First App'),
+          title: const Text('My First App'),
         ),
         body: Column(
           children: <Widget>[
-            Text(questions[questionIndex]),
+            Question(
+              questions[_questionIndex],
+            ),
             ElevatedButton(
               onPressed: () => answerQuestion(),
               child: Text('Answer 1'),
